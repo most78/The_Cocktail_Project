38cc29ee33bdd540ff754471cc60e292a10072c5
Marcos Ostos <marcos.ostos@gmail.com>
2017-10-20 20:09:00 +0200

    add pug

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const extractSass = new ExtractTextPlugin({
@@ -7,7 +8,7 @@ const extractSass = new ExtractTextPlugin({
 });
 
 module.exports = {
-  entry: ['./src/index.js', './src/scss/main.scss'],
+  entry: ['./src/index.js', './src/scss/main.scss',],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
@@ -17,7 +18,11 @@ module.exports = {
       {
         test: /\.(sass|scss)$/,
         loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
-      }
+      },
+      {
+        test: /\.pug$/,
+        loaders: ['html-loader', 'pug-html-loader'],
+      },
     ]
   },
   plugins: [
@@ -25,6 +30,12 @@ module.exports = {
     new ExtractTextPlugin({
       filename: 'style.css',
       allChunks: true,
+    }),
+    new HtmlWebpackPlugin({
+      title: 'The_Cocktail_Project',
+      minify:{collapseWhitespace: false},
+      hash: true,
+      template: './src/pug/main.pug',
     }),
   ],
 };
